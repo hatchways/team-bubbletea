3fb6133ad90660545d8af4af0a1ce108e735c0f7
john-rigas <jmr294@georgetown.edu>
2019-11-18 13:24:48 +0000

    added requirements.txt and sql alchemy setup

--- server/app.py
+++ server/app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from api.ping_handler import ping_handler
 from api.home_handler import home_handler
+from flask_sqlalchemy import SQLAlchemy
+from config import POSTGRES_DATABASE, POSTGRES_PASSWORD, POSTGRES_URL, POSTGRES_USERNAME
 
+username = 'postgres'
+password = 'postgres'
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql+psycopg2://{POSTGRES_USERNAME}:{POSTGRES_PASSWORD}@{POSTGRES_URL}/{POSTGRES_DATABASE}'
+db = SQLAlchemy(app)
 
 app.register_blueprint(home_handler)
 app.register_blueprint(ping_handler)
-
