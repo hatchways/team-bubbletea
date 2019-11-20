8aa72b28904e898d3232614d1532d3969031fa49
john-rigas <jmr294@georgetown.edu>
2019-11-20 17:42:05 +0000

    fixed submission controller get queries

--- server/api/submission_handler.py
+++ server/api/submission_handler.py
@@ -38,13 +38,13 @@ def show_submission(contest_id, submission_id):
 # dedicated page for editing a contest, could potentially be combined with show_contest()
 @submission_handler.route('/<int:submission_id>/edit')
 def edit(contest_id, submission_id):
-    submission = Submission.query.get_or_404(id)
+    submission = Submission.query.get_or_404(submission_id)
     return jsonify('Show form for editing this submission', submission.to_dict())
 
 
 @submission_handler.route('/<int:submission_id>', methods=['PATCH', 'PUT'])
 def update(contest_id, submission_id):
-    submission = Submission.query.get_or_404(id)
+    submission = Submission.query.get_or_404(submission_id)
     # may be a better way to do this than setattr
     for key in request.json.keys():
         setattr(submission, key, request.json[key])
@@ -54,7 +54,7 @@ def update(contest_id, submission_id):
 
 @submission_handler.route('/<int:submission_id>', methods=['DELETE'])
 def delete(contest_id, submission_id):
-    submission = Submission.query.get_or_404(id)
+    submission = Submission.query.get_or_404(submission_id)
     db.session.delete(submission)
     db.session.commit()
     return redirect(url_for('submission_handler.show_all', contest_id=contest_id))
