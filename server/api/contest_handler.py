8825bc1beb1ca66ffd80a92c33879c353faa9571
john-rigas <jmr294@georgetown.edu>
2019-11-20 13:44:50 +0000

    added get or 404 to contest handler

--- server/api/contest_handler.py
+++ server/api/contest_handler.py
@@ -39,19 +39,19 @@ def create():
 
 @contest_handler.route('/<int:id>')
 def show_contest(id):
-    contest = Contest.query.get(id)
+    contest = Contest.query.get_or_404(id)
     return jsonify(contest.to_dict())
 
 # dedicated page for editing a contest, could potentially be combined with show_contest()
 @contest_handler.route('/<int:id>/edit')
 def edit(id):
-    contest = Contest.query.get(id)
+    contest = Contest.query.get_or_404(id)
     return jsonify('Show form for editing this contest', contest.to_dict())
 
 
-@contest_handler.route('/<int:id>', methods=['POST', 'PATCH', 'PUT'])
+@contest_handler.route('/<int:id>', methods=['PATCH', 'PUT'])
 def update(id):
-    contest = Contest.query.get(id)
+    contest = Contest.query.get_or_404(id)
     input_data = request.json
     # may be a better way to do this than setattr
     for key in input_data.keys():
@@ -62,7 +62,7 @@ def update(id):
 
 @contest_handler.route('/<int:id>', methods=['DELETE'])
 def delete(id):
-    contest = Contest.query.get(id)
+    contest = Contest.query.get_or_404(id)
     db.session.delete(contest)
     db.session.commit()
     return redirect(url_for('contest_handler.show_all'))
