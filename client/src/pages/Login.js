4f8eacba85dc2158948d9bac1fe9ae3df3088eeb
Ronald <30670000+rwhs@users.noreply.github.com>
2019-11-21 13:22:36 -0500

    working on login frontend

---
+++ client/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+// import { Route, Link } from "react-router-dom";
+
+class Login extends Component {
+    render() {
+        return (
+            <h1>Login test</h1>
+        )
+    }
+}
+
+export default Login;
