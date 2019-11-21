a8197ef86c8ca1247ef2e2eb39f9f5cea729da99
Ronald <30670000+rwhs@users.noreply.github.com>
2019-11-21 13:22:28 -0500

    changed frontend routing so only one page renders at a time

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { MuiThemeProvider } from "@material-ui/core";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import { theme } from "./themes/theme";
 import LandingPage from "./pages/Landing";
+import Login from "./pages/Login";
 
 import "./App.css";
 
@@ -11,7 +12,10 @@ function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
-        <Route path="/" component={LandingPage} />
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/login" component={Login} />
+        </Switch>
       </BrowserRouter>
     </MuiThemeProvider>
   );
