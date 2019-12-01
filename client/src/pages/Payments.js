8d8602ccab938644dc5a35619c310e94984a6db1
john-rigas <jmr294@georgetown.edu>
2019-12-01 21:39:36 +0000

    added refund button

--- client/src/pages/Payments.js
+++ client/src/pages/Payments.js
@@ -77,6 +77,18 @@ var Payments = function () {
         })();
     }
 
+    function refundPayment() {
+        (async () => {
+            const response = await fetch(`/users/${userID}/payments/cc/refund`, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ payment_intent_id: 'pi_1FkzvgCTioA7mNziCEymRDXa' })
+            });
+        })();
+    }
 
     return (
         <div>
@@ -99,6 +111,9 @@ var Payments = function () {
             </div>
             <div>
                 <button type="submit" onClick={makePayment}>Make Payment</button>
+            </div>
+            <div>
+                <button type="submit" onClick={refundPayment}>Refund Payment</button>
             </div>
         </div>
     );
