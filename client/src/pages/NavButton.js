b4e5009df4d2fe9e6ec75b7a7c54ac4cd387d944
Bonnie Li <bonnie@hatchways.io>
2019-12-03 09:52:30 -0500

    Componentize Button

---
+++ client/src/pages/NavButton.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1)
+  },
+  title: {
+    fontFamily: theme.fontFamily,
+    fontSize: 10,
+    padding: "5px"
+  }
+}));
+
+export function NavButton({ buttonName }) {
+  const classes = useStyles();
+
+  return (
+    <Button color="inherit" className={classes.button}>
+      <Typography className={classes.title} variant="button">
+        {buttonName}
+      </Typography>
+    </Button>
+  );
+}
