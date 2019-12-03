443e3a76ee7ec4b434b7b4945c2cf2907ec43298
Bonnie Li <bonnie@hatchways.io>
2019-12-03 10:13:40 -0500

    Simplify the margins on this page

--- client/src/pages/ContestDetailsPaper.js
+++ client/src/pages/ContestDetailsPaper.js
@@ -1,28 +1,13 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Paper, Typography, Grid } from '@material-ui/core';
-import { BackToContestsListButton } from './BackToContestsListsButton';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Paper, Typography, Grid } from "@material-ui/core";
+import { BackToContestsListButton } from "./BackToContestsListsButton";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    "padding-bottom": "1%",
-    "padding-top": "0.5%",
-    "box-shadow": null, 
-  },
-  title: {
-    "margin-left": "8.5%",
-    display: "flex"
-  },
-  prize: {
-    "margin-left": "25%",
-    "margin-right": "65%",
-  },
-  button: {
-    "margin-left": "8.5%"
-  },
-  // titleDiv: {
-  //   display: "flex", 
-  // }
+    boxShadow: null,
+    padding: theme.spacing(2)
+  }
 }));
 
 export function ContestDetailsPaperSheet() {
@@ -30,28 +15,18 @@ export function ContestDetailsPaperSheet() {
 
   return (
     <Grid container justify="center">
-        <Grid item xs={9} md={7} lg={12} className={classes.position}>
-            <Paper className={classes.root}>
-              <Grid item className={classes.button}>
-                <BackToContestsListButton />
-              </Grid>
-              <Grid item className={classes.title}>
-                <div className={classes.titleDiv}>
-                  <Typography variant="h4">
-                    Contest Title
-                  </Typography>
-                  <Typography variant="h6">
-                    $150
-                  </Typography>
-                </div>
-              </Grid>
-              <Grid item className={classes.title}>
-                <Typography variant="caption">
-                  Author name.
-                </Typography>
-              </Grid>
-            </Paper>
-        </Grid>
+      <Grid item xs={9} md={7} lg={12} className={classes.position}>
+        <Paper className={classes.root}>
+          <BackToContestsListButton />
+
+          <div>
+            <Typography variant="h4">Contest Title</Typography>
+            <Typography variant="h6">$150</Typography>
+          </div>
+
+          <Typography variant="caption">Author name.</Typography>
+        </Paper>
+      </Grid>
     </Grid>
   );
 }
