5f1c23066f35127b68477348f3cab0f500fe076f
Mazia Syed <maziasyed@gmail.com>
2019-12-03 22:21:04 -0500

    changed for loop to map

--- client/src/pages/ViewSubmissions.js
+++ client/src/pages/ViewSubmissions.js
@@ -2,38 +2,34 @@ import React from 'react';
 import { Image } from './Image';
 
 export class ViewSubmissions extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			submissions: [], 
-			submissionKeys: []
-		}
-   	this.viewAllSubmissions = this.viewAllSubmissions.bind(this)
-	}
+  constructor(props) {
+    super(props)
+    this.state = {
+      submissions: [],
+      submissionKeys: []
+    }
+    this.viewAllSubmissions = this.viewAllSubmissions.bind(this)
+  }
 
-	componentDidMount() {
+  componentDidMount() {
     this.viewAllSubmissions()
-  	}
+  }
 
-  	viewAllSubmissions() {
-    	fetch('/contests/2/submissions').then(response =>
-      	response.json().then(data => {
-        	this.setState({ submissions: data.files, submissionKeys: data.fileKeys }); 
-      		})
-    	) 
-  	}
+  viewAllSubmissions() {
+    fetch('/contests/2/submissions').then(response =>
+      response.json().then(data => {
+        this.setState({ submissions: data.files, submissionKeys: data.fileKeys });
+      })
+    )
+  }
 
-	render() {
-		let imagesArray = []
-		for(let i=0; i < this.state.submissions.length; i++) {
-			imagesArray.push(
-				<Image imageURL={this.state.submissions[i]} imageKey={this.state.submissionKeys[i]} key={i}/>
-			)
-		}
-		return (
-			<div>
-				{imagesArray}
-			</div>
-			)
-		}
+  render() {
+    return (
+      <div>
+        {this.state.submissions.map((submission, i) => (
+          <Image imageURL={submission} imageKey={this.state.submissionKeys[i]} key={i} />
+        ))}
+      </div>
+    )
+  }
 }
