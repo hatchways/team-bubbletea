9d5837f3c84833812c5c952b84afb5f6e3bd381f
john-rigas <jmr294@georgetown.edu>
2019-12-05 20:18:03 +0000

    commented out manual tests

--- server/test/contest_handler_test.py
+++ server/test/contest_handler_test.py
@@ -7,22 +7,19 @@ import json
 
 
 class ContestHandlerTest(TestBase):
-    # manual test for now
-    def test_create_contest(self):
-        input_data = {'title': 'lion'+str(random.randint(0, 10000000)),
-                      'description': 'any old lion tattoo',
-                      'prize': 27.50,
-                      'deadline': datetime(2019, 12, 12, 12, 12).strftime("%m/%d/%Y, %H:%M:%S")}
+    pass
 
-        self.api.post('/contests', data=json.dumps(input_data),
-                      content_type='application/json')
+    # def test_create_contest(self):
+    #     input_data = {'title': 'lion'+str(random.randint(0, 10000000)),
+    #                   'description': 'any old lion tattoo',
+    #                   'prize': 27.50,
+    #                   'deadline': datetime(2019, 12, 12, 12, 12).strftime("%m/%d/%Y, %H:%M:%S")}
 
-    # manual test for now
-    def test_update_contest(self):
-        self.api.post(
-            '/contests/1',
-            data=json.dumps({'prize': random.randint(0, 100000)}),
-            content_type='application/json')
+    #     self.api.post('/contests', data=json.dumps(input_data),
+    #                   content_type='application/json')
 
-    if __name__ == "__main__":
-        unittest.main()
+    # def test_update_contest(self):
+    #     self.api.post(
+    #         '/contests/1',
+    #         data=json.dumps({'prize': random.randint(0, 100000)}),
+    #         content_type='application/json')
