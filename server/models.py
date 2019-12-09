7ec727abce84a7082d2a3acedeb1adbbc494f0c9
Ronald <30670000+rwhs@users.noreply.github.com>
2019-12-08 23:23:38 -0500

    add email and password columns to user table

--- server/models.py
+++ server/models.py
@@ -8,6 +8,8 @@ class User(db.Model):
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
     contests = db.relationship('Contest', backref='owner', lazy=True)
     submissions = db.relationship('Submission', backref='artist', lazy=True)
     stripe_transfer_id = db.Column(db.String)
