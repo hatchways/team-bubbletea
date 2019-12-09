377da872fc6c8322a5fbceca39485aebf49316ae
Ronald <30670000+rwhs@users.noreply.github.com>
2019-12-08 23:56:41 -0500

    basic frontend for signup

---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,38 @@
+import React, { useState, Fragment } from 'react';
+import { Header } from "./Header";
+import { SignUpButton } from "./SignUpButton";
+
+export default function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [result, setResult] = useState("")
+
+  const handleSignupSubmit = (e) => {
+    fetch('http://localhost:5000/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "email": email,
+        "password": password
+      })
+    })
+    .then(response => response.json())
+    .then(response => {
+      setResult(response.result)
+    })
+  }
+
+  return (
+    <Fragment>
+      <Header>
+        <SignUpButton />
+      </Header>
+      <input placeholder="Email" value={email} onChange={e => setEmail(e.target.value)}></input>
+      <input placeholder="Password" type="password" value={password} onChange={e => setPassword(e.target.value)}></input>
+      <button type="submit" onClick={handleSignupSubmit}>Submit</button>
+      <p>{result}</p>
+    </Fragment>
+  )
+}
