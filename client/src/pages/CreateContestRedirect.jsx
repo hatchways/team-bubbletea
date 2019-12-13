fdd029efa927d519ed6902d2cb3cdb2bdcb3d63f
Ronald <30670000+rwhs@users.noreply.github.com>
2019-12-13 14:40:47 -0500

    quick fixes, added redirect button to create contest

---
+++ client/src/pages/CreateContestRedirect.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { makeStyles, Button, Typography, Grid } from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    fontFamily: theme.fontFamily,
+  },
+  button: {
+    backgroundColor: "black",
+    color: theme.bgcolor,
+    borderRadius: "0",
+    padding: theme.spacing(4, 10)
+  },
+  buttonContainer: {
+    marginTop: "50px"
+  }
+}));
+
+export function CreateContestRedirect(props) {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Grid container justify="center">
+        <div className={classes.buttonContainer}>
+          <Button type={props.type} className={classes.button} variant="contained" onClick={props.onClick}>
+            <Typography className={classes.root}>
+              Create Contest
+      		  </Typography>
+          </Button>
+        </div>
+      </Grid>
+    </div>
+  )
+}
